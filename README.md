05ece2408414ad6f4d2c2c548bffaff6f81379ae
rossa <fashionfish@naver.com>
2021-07-05 21:27:21 +0900

    4장 반응속도체크 Hooks로 전환하기

--- README.md
+++ README.md
@@ -37,3 +37,4 @@
 - [x] React 조건문
 - [x] setTimeout 넣어 반응속도체크
 - [x] 리셋 버튼
+- [x] 반응속도체크 Hooks로 전환하기
--- ch04/client.jsx
+++ ch04/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ResponseCheck from "./ResponseCheckClass";
+import ResponseCheck from "./ResponseCheck";
 
 ReactDOM.render(<ResponseCheck />, document.getElementById("root"));
