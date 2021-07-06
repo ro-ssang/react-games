62954430f118529e885b6b3b78086586d962a8be
rossa <fashionfish@naver.com>
2021-07-06 14:51:15 +0900

    5장 고차 함수

--- ch05/RSPClass.jsx
+++ ch05/RSPClass.jsx
@@ -53,7 +53,7 @@ class RSP extends Component {
         }
     }
 
-    onClickBtn = (choice) => {
+    onClickBtn = (choice) => () => {
         const { imgCoord } = this.state;
         clearTimeout(this.interval);
         const myScore = scores[choice];
@@ -89,9 +89,9 @@ class RSP extends Component {
             <>
                 <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0` }}></div>
                 <div>
-                    <button id="rock" className="btn" onClick={() => this.onClickBtn("바위")}>바위</button>
-                    <button id="scissors" className="btn" onClick={() => this.onClickBtn("가위")}>가위</button>
-                    <button id="paper" className="btn" onClick={() => this.onClickBtn("보")}>보</button>
+                    <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>바위</button>
+                    <button id="scissors" className="btn" onClick={this.onClickBtn("가위")}>가위</button>
+                    <button id="paper" className="btn" onClick={this.onClickBtn("보")}>보</button>
                 </div>
                 <div>{result}</div>
                 <div>현재 {score}점</div>
