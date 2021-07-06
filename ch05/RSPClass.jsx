8c024521e1d7bcc4b3247629d0adce2321cb4010
rossa <fashionfish@naver.com>
2021-07-06 14:01:43 +0900

    5장 setInterval과 라이프사이클 연동하기

--- ch05/RSPClass.jsx
+++ ch05/RSPClass.jsx
@@ -7,23 +7,40 @@ constructor -> render -> ref -> componentDidMount
 부모가 나를 없앴을 때 -> componentWillUnmount
 */
 
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+};
+
 class RSP extends Component {
     state = {
         result: '',
-        imgCoord: 0,
+        imgCoord: '0',
         score: 0,
     };
 
-    componentDidMount() { // 컴포넌트가 첫 렌더링된 후
+    interval;
 
+    componentDidMount() { // 컴포넌트가 첫 렌더링된 후, 여기에 비동기 요청을 많이 한다.
+        this.interval = setInterval(() => {
+            const { imgCoord } = this.state;
+            if (imgCoord === rspCoords.바위) {
+                this.setState({ imgCoord: rspCoords.가위 });
+            } else if (imgCoord === rspCoords.가위) {
+                this.setState({ imgCoord: rspCoords.보 });
+            } else if (imgCoord === rspCoords.보) {
+                this.setState({ imgCoord: rspCoords.바위 });
+            }
+        }, 1000);
     }
 
     componentDidUpdate() { // 리렌더링 후
 
     }
 
-    componentWillUnmount() { // 컴포넌트가 제거되기 직전
-
+    componentWillUnmount() { // 컴포넌트가 제거되기 직전, 비동기 요청 정리를 많이 한다.
+        clearInterval(this.interval);
     }
 
     render() {
